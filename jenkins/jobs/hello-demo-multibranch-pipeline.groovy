75293ab5ad12cf24b7511a7d7492a66ff0435cb4
hIftikhar <hisam.iftikhar@digital.homeoffice.gov.uk>
2026-04-30 16:26:39 +0100

    Updated job

--- jenkins/jobs/hello-demo-multibranch-pipeline.groovy
+++ jenkins/jobs/hello-demo-multibranch-pipeline.groovy
@@ -6,11 +6,33 @@ multibranchPipelineJob('hello-demo') {
                     id('hello-demo-id')
                     repoOwner('Quashed')
                     repository('hello-demo')
+//                    scanCredentialsId('github')
                     traits {
                         gitBranchDiscovery()
                     }
                 }
             }
+
+            strategy {
+                defaultBranchPropertyStrategy {
+                    props {
+                        noTriggerBranchProperty()
+                    }
+                }
+            }
+
+            buildStrategies {
+                buildRegularBranches()
+                buildChangeRequests {
+                    ignoreTargetOnlyChanges(true)
+                }
+            }
+        }
+    }
+
+    triggers {
+        periodicFolderTrigger {
+            interval('1h')
         }
     }
 
